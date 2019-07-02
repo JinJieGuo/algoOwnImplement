23385935ca64ebda88149f15d3a83afdacd302cd
guojinjie <644574614@qq.com>
2019-07-02 14:12:12 +0800

    leetcode 70 爬楼梯 ✅ (添加非递归写法)

--- src/main/java/ownCode/recursion/TestRecursion.java
+++ src/main/java/ownCode/recursion/TestRecursion.java
@@ -11,16 +11,36 @@ public class TestRecursion {
 
 
 
-
+    //递归写法
     public static int f(int n){
         if(n==1) return 1;
         if(n==2) return 2;
         return f(n-1) + f(n-2);
     }
 
+    //非递归写法
+    public static int  g(int n){
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        int result = 0;
+        int pre = 2;
+        int prepre = 1;
+
+        for (int i = 3; i <=n ; i++) {
+            result = pre + prepre;
+            prepre = pre;
+            pre = result;
+        }
+        return result;
+    }
+
+
     public static void main(String[] args) {
         int f = f(10);
+        int g = g(10);
         System.out.println(f);
+        System.out.println(g);
 
     }
 
